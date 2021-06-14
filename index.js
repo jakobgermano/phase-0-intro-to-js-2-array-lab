25bd79ec5cd9abacc54494528e936299a2afb15d
jakob <jakob.germano@gmail.com>
2021-06-14 17:39:08 -0600

    commit tree 58fe4924130670355a409cec12b7ae2f8da9f719

--- index.js
+++ index.js
@@ -21,13 +21,16 @@ function appendCat(name) {
     return [...cats, name];
 }
 
+
 function prependCat(name) {
     return [name, ...cats];
 }
 
+
 function removeLastCat() {
     return cats.slice(0, cats.length - 1);
 }
+
 
 function removeFirstCat() {
     return cats.slice(cats.length - 2)
